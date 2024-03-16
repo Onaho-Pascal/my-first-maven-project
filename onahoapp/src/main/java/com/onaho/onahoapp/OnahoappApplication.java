12bee4764e957be67e261b457697cab33f6dd49f
Pascal <Pascalonaho@gmail.com>
2024-03-16 15:29:30 +0100

    my third maven project

---
+++ onahoapp/src/main/java/com/onaho/onahoapp/OnahoappApplication.java
@@ -0,0 +1,13 @@
+package com.onaho.onahoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnahoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnahoappApplication.class, args);
+	}
+
+}
---
+++ onahoapp/src/test/java/com/onaho/onahoapp/OnahoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onaho.onahoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnahoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
