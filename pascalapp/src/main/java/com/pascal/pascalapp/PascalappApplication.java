a73a4162d45a606be8ce0329ab3c153e2af665cc
Pascal <Pascalonaho@gmail.com>
2024-03-16 13:40:34 +0100

    my first maven project

---
+++ pascalapp/src/main/java/com/pascal/pascalapp/PascalappApplication.java
@@ -0,0 +1,13 @@
+package com.pascal.pascalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PascalappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PascalappApplication.class, args);
+	}
+
+}
---
+++ pascalapp/src/test/java/com/pascal/pascalapp/PascalappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pascal.pascalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PascalappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
