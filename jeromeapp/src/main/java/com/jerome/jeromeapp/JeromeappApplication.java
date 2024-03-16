42e6bf4e79f48fc3abca35f9e43cffdbdf13ce36
Pascal <Pascalonaho@gmail.com>
2024-03-16 15:12:57 +0100

    my second maven project

---
+++ jeromeapp/src/main/java/com/jerome/jeromeapp/JeromeappApplication.java
@@ -0,0 +1,13 @@
+package com.jerome.jeromeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeromeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeromeappApplication.class, args);
+	}
+
+}
---
+++ jeromeapp/src/test/java/com/jerome/jeromeapp/JeromeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerome.jeromeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeromeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
